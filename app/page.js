880b9ced811158f8b019887ec2c1701ef90e5142
edwin141999 <gendevilman@gmail.com>
2023-07-03 14:10:19 -0600

    update main

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import About from "./about/page";
+import Footer from "./footer/page";
 import Header from "./header/page";
 import Projects from "./sections/page";
 
@@ -8,34 +9,7 @@ export default function Home() {
       <Header />
       <About />
       <Projects />
-      <footer className="min-h-screen min-w-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 text-white flex flex-col justify-center py-10">
-        <h2 className="text-4xl font-bold text-center">CONTACT</h2>
-        <div className="bg-white rounded-md text-black m-auto px-8 space-y-2 py-4 flex flex-col">
-          <p className="font-bold">Name</p>
-          <input
-            type="text"
-            className="border-2 rounded-md px-2"
-            placeholder="Enter your name"
-          />
-          <p className="font-bold">Email</p>
-          <input
-            type="text"
-            className="border-2 rounded-md px-2"
-            placeholder="Enter your email address"
-          />
-          <p className="font-bold">Message</p>
-          <input
-            type="text"
-            className="border-2 rounded-md px-2"
-            placeholder="Enter your message"
-          />
-          <button className="text-xl py-0.5 px-5 borderbtn w-fit mx-auto">
-            <span className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-clip-text text-transparent">
-              Send
-            </span>
-          </button>
-        </div>
-      </footer>
+      <Footer />
     </main>
   );
 }
